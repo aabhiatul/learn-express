bc2ffa2d0929a962d0d0ba8d8fbd019f7ee20bb8
Aabhi_atul <aabhi_atul@Aabhi-atuls-MacBook-Air.local>
2023-04-07 23:37:02 +0530

    added html to app

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const app = express();
 
 app.get('/home',(req,res)=>{
     console.log('req from user -', req.query.name)
-   res.send("<h1>express server!</h2>")    
+   res.send(`
+   <h1>express server!</h2>
+   <a href='/about'>go to home</button>
+   `)    
 });
+app.get('/about',(req,res)=>{
+    res.send(`
+    <input type="text" value=${req?.query?.name}/><a href='/home'>go to home</button>
+    `)
+})
 
 app.listen(8000,(req,res)=>{
     console.log('running at port 8000!')
