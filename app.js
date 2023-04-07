63abd1aa81d593984ae23cfcd4d2097dc9b46eed
Aabhi_atul <aabhi_atul@Aabhi-atuls-MacBook-Air.local>
2023-04-08 00:02:09 +0530

    added html files rendering

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express')
+const path = require('path')
+
 const app = express();
 
-app.get('/home',(req,res)=>{
-    console.log('req from user -', req.query.name)
-   res.send(`
-   <h1>express server!</h2>
-   <a href='/about'>go to home</button>
-   `)    
-});
+// app.get('/home',(req,res)=>{
+//     console.log('req from user -', req.query.name)
+//    res.send(`
+//    <h1>express server!</h2>
+//    <a href='/about'>go to home</button>
+//    `)    
+// });
+// app.get('/about',(req,res)=>{
+//     res.send(`
+//     <input type="text" value=${req?.query?.name}/><a href='/home'>go to home</button>
+//     `)
+// })
+const publicPath = path.join(__dirname,'public')
+// app.use(express.static(publicPath))
+app.get('',(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+
 app.get('/about',(req,res)=>{
-    res.send(`
-    <input type="text" value=${req?.query?.name}/><a href='/home'>go to home</button>
-    `)
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/home',(req,res)=>{
+    res.sendFile(`${publicPath}/home.html`)
+})
+app.get('/*',(req,res)=>{
+    res.sendFile(`${publicPath}/404.html`)
 })
 
 app.listen(8000,(req,res)=>{
