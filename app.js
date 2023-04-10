ba9e4c62d48410ecb5ea446be639afd093694741
Aabhi_atul <aabhi_atul@Aabhi-atuls-MacBook-Air.local>
2023-04-11 00:17:04 +0530

    added middleware

--- app.js
+++ app.js
@@ -15,22 +15,45 @@ const app = express();
 //     <input type="text" value=${req?.query?.name}/><a href='/home'>go to home</button>
 //     `)
 // })
-const publicPath = path.join(__dirname,'public')
-// app.use(express.static(publicPath))
-app.get('',(req,res)=>{
-    res.sendFile(`${publicPath}/index.html`)
+// const publicPath = path.join(__dirname,'public')
+// // app.use(express.static(publicPath))
+// app.get('',(req,res)=>{
+//     res.sendFile(`${publicPath}/index.html`)
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.sendFile(`${publicPath}/about.html`)
+// })
+
+// app.get('/home',(req,res)=>{
+//     res.sendFile(`${publicPath}/home.html`)
+// })
+// app.get('/*',(req,res)=>{
+//     res.sendFile(`${publicPath}/404.html`)
+// })
+
+
+// ----Middleware----
+
+const fillterUser = (req,res,next) => {
+    if(!req.query.age){
+        res.send('Access denied')
+    }else if(req.query.age < 18){
+        res.send('bellow 18')
+    }else{
+        next()
+    }
+}
+
+app.use(fillterUser)
+
+app.get('/',(req,res)=>{
+    res.send('Hi there it is home page')
+})
+app.get('/user',(req,res)=>{
+    res.send('this is user page!')
 })
 
-app.get('/about',(req,res)=>{
-    res.sendFile(`${publicPath}/about.html`)
-})
-
-app.get('/home',(req,res)=>{
-    res.sendFile(`${publicPath}/home.html`)
-})
-app.get('/*',(req,res)=>{
-    res.sendFile(`${publicPath}/404.html`)
-})
 
 app.listen(8000,(req,res)=>{
     console.log('running at port 8000!')
